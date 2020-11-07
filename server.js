1c1afbcbf0fcd1c36fcb3fb22e9731d6b40b3d94
Glitch (noble-phase-leaf) <none>
2020-11-07 04:29:29 +0000

    💘🌞 Checkpoint ./server.js:21184821/3567

--- server.js
+++ server.js
@@ -1,67 +1,65 @@
-'use strict';
+"use strict";
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+var express = require("express");
+var bodyParser = require("body-parser");
+var expect = require("chai").expect;
+var cors = require("cors");
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+var apiRoutes = require("./routes/api.js");
+var fccTestingRoutes = require("./routes/fcctesting.js");
+var runner = require("./test-runner");
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-app.get('/api/convert',(req,res)=>{
+//Routing for API
+app.get("/api/convert", (req, res) => {
+  var input = req.query.input;
+  var input1 = "127km.25gb";
+
+  var unitRegex = /[a-z]*$/i;
+  var units = input1.match(unitRegex)[0];
   
-  var input = req.query.input
-  var input1 = '127km.25gb'
+  var 
+  console.log(units);
+});
 
-  var regex = /[a-z]*/gi
-  var units = input1.match(regex)
-  var unit = units.filter((el)=>{
-    return el !== ''
-  })
-  console.log(unit)
-})
+apiRoutes(app);
 
-apiRoutes(app);  
-    
 //404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+  res
+    .status(404)
+    .type("text")
+    .send("Not Found");
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log("Tests are not valid:");
+        console.log(error);
       }
     }, 1500);
   }
