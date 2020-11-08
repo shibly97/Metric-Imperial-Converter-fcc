6fb7b3c7166c08fcabd713cc541df972400a480e
Glitch (noble-phase-leaf) <none>
2020-11-08 05:22:35 +0000

    📅😻 Checkpoint ./controllers/convertHandler.js:21184821/144

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -142,7 +142,7 @@ function ConvertHandler() {
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
     
-    
+    result = initNum+' '+this.spellOutUnit(initUnit)+' converts to '+returnNum+' '+this.spellOutUnit(returnUnit)
     
     return result;
   };
